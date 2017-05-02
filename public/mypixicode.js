eaf528469bfc9fd399ba38dae30e61612bb2709e
Jon Lake <francis.j.lake@gmail.com>
2017-05-02 14:51:14 -0400

    Added mypixicode.js.

---
+++ public/mypixicode.js
@@ -0,0 +1,11 @@
+// Create the renderer.
+const renderer = PIXI.autoDetectRenderer(256, 256);
+
+// Add the canvas to the HTML document.
+document.body.appendChild(renderer.view);
+
+// Create a container object called the 'stage'.
+const stage = new PIXI.Container();
+
+// Tell the renderer to render the stage.
+renderer.render(stage);
